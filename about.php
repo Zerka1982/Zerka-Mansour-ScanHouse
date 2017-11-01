81418dd037f2875909e22e3fffd7b74a575dbc56
Youssef Zerkani <you.zerkani@gmail.com>
2017-11-01 23:09:43 +0200

    adding some changes

--- about.php
+++ about.php
@@ -18,7 +18,7 @@ include "src/header.php";
 
   <!--  *******Steps*******  -->
   <section class="about-section">
-    <div class="about-icons wow zoomIn">
+    <div class="about-icons">
       <i  id="" class="fa fa-home" aria-hidden="true" style="font-size: 80px; color: #7BCABB;"></i> 
       <div class="style2"></div>
       <i class="fa fa-user-circle-o" aria-hidden="true" style="font-size: 100px; color: #7BCABB; background: transparent;"></i>
@@ -26,20 +26,20 @@ include "src/header.php";
       <i id="" class="fa fa-hourglass-half" aria-hidden="true" style="font-size: 80px; color: #7BCABB; display: block;"></i>
     </div>
     <div class="description">
-      <div class="description-icon wow zoomIn">
+      <div class="description-icon">
         <h2>Step 1</h2> 
         <h1>Write check list</h1>
         <p>Write check lists about things you want to know about house
         for example, the location, neighbor, etc.</p>
       </div>
-      <div class="description-icon wow zoomIn">
+      <div class="description-icon">
         <h2>Step 2</h2> 
         <h1>Select a host</h1>
         <p>You can choose the host. All of them are trustful locals who are 
         well known the city you <br>want to live. You can see reviews and top 
         ranking hosts before choosing</p>
       </div>
-      <div class="description-icon wow zoomIn">
+      <div class="description-icon">
         <h2>Step 3</h2> 
         <h1>Wait for the response</h1>
         <p>As soon as you're done with every steps, host will contact you. It won't take time more than 3 days.</p>
@@ -48,32 +48,32 @@ include "src/header.php";
   </section>
 
   <section class="photo">
-    <div class="content wow zoomIn">
+    <div class="content">
       <p>As locals, we try our best to give you best information.</p>
     </div>   
   </section>
 
   <!--  *******Process*******  -->
   <section class="check">
-    <div class="content wow zoomIn">
+    <div class="content">
       <h1>House check process</h1>
       <p>Host will provide you with 2 reports, one skype meeting.</p>
       <div class="main-content">
-        <div class="option wow fadeInLeft" style="background: grey">
+        <div class="option" style="background: grey">
           <div class="optionI">
             <i class="fa fa-file-text fa-3x"></i>
           </div>
           <h2>Brief Report</h2>
           <p>First brief report after host visited houses.</p>
         </div>
-        <div class="option wow fadeInDown" style="background: #7BCABB">
+        <div class="option" style="background: #7BCABB">
           <div class="optionI">
             <i class="fa fa-television fa-3x"></i>
           </div>
           <h2>Skype Meeting</h2>
           <p>Set the date for Skype-call with host.</p>
         </div>
-        <div class="option wow fadeInRight" style="background: grey"> 
+        <div class="option" style="background: grey"> 
           <div class="optionI">
           <i class="fa fa-file-text fa-3x"></i>
           </div>
