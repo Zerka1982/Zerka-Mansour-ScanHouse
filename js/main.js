80c0564ae2928ba33ee062092839d7e268e71611
Youssef Zerkani <you.zerkani@gmail.com>
2017-11-21 13:28:47 +0200

    changing the font style and adding the modal

--- js/main.js
+++ js/main.js
@@ -7,13 +7,13 @@
 
 // (function($) {
 
-// 	"use strict";
+//  "use strict";
 
 
 //    $(window).load(function() {
 
-// 	/*-----------------------------------------------------*/
-//   	/* Mobile Menu
+//  /*-----------------------------------------------------*/
+//    /* Mobile Menu
 //    ------------------------------------------------------ */  
      $(document).ready(function(){
                   $(".menu-toggle a").click(function(){ 
@@ -23,45 +23,45 @@
             });
 
 
-// 	/*----------------------------------------------------*/
-//   	/* Smooth Scrolling
-//   	------------------------------------------------------*/
-//   	$('.smoothscroll').on('click', function (e) {
-	 	
-// 	 	e.preventDefault();
+//  /*----------------------------------------------------*/
+//    /* Smooth Scrolling
+//    ------------------------------------------------------*/
+//    $('.smoothscroll').on('click', function (e) {
+    
+//    e.preventDefault();
 
-//    	var target = this.hash,
-//     	$target = $(target);
+//      var target = this.hash,
+//      $target = $(target);
 
-//     	$('html, body').stop().animate({
-//        	'scrollTop': $target.offset().top
+//      $('html, body').stop().animate({
+//          'scrollTop': $target.offset().top
 //       }, 800, 'swing', function () {
-//       	window.location.hash = target;
+//        window.location.hash = target;
 //       });
 
-//   	});  
+//    });  
 
 
-//  	/*----------------------------------------------------- */
-//   	/* Back to top
+//    /*----------------------------------------------------- */
+//    /* Back to top
 //    ------------------------------------------------------- */ 
-// 	var pxShow = 300; // height on which the button will show
-// 	var fadeInTime = 400; // how slow/fast you want the button to show
-// 	var fadeOutTime = 400; // how slow/fast you want the button to hide
-// 	var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+//  var pxShow = 300; // height on which the button will show
+//  var fadeInTime = 400; // how slow/fast you want the button to show
+//  var fadeOutTime = 400; // how slow/fast you want the button to hide
+//  var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
 
 //    // Show or hide the sticky footer button
-// 	jQuery(window).scroll(function() {
+//  jQuery(window).scroll(function() {
 
-// 		if (!( $("#header-search").hasClass('is-visible'))) {
+//    if (!( $("#header-search").hasClass('is-visible'))) {
 
-// 			if (jQuery(window).scrollTop() >= pxShow) {
-// 				jQuery("#go-top").fadeIn(fadeInTime);
-// 			} else {
-// 				jQuery("#go-top").fadeOut(fadeOutTime);
-// 			}
-// 		}		
-// 	});		
+//      if (jQuery(window).scrollTop() >= pxShow) {
+//        jQuery("#go-top").fadeIn(fadeInTime);
+//      } else {
+//        jQuery("#go-top").fadeOut(fadeOutTime);
+//      }
+//    }   
+//  });   
 // });
 // })(jQuery);
 
@@ -98,23 +98,41 @@ $(window).scroll(function(){
 
       // Get the modal
 var modal = document.getElementById('myModal');
+var part1 = document.getElementById("part1");
+var part2 = document.getElementById("part2");
 
 // Get the button that opens the modal
 var btn = document.getElementById("singIn");
 
+var btnMin = document.getElementById("singInM");
+
+var btnMup = document.getElementById("singUpM");
+
 // Get the <span> element that closes the modal
 var closeBtn = document.getElementsByClassName("x-button");
-
+var closeBtnM = document.getElementById("closeM");
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
+    part1.style.display="block";
+    part2.style.display="inline-block"    
 }
-
+btnMin.onclick = function() {
+    modal.style.display = "block";
+    part1.style.display="block";
+    part2.style.display="none"    
+}
+btnMup.onclick = function() {
+    modal.style.display = "block";
+    part2.style.display="block"    
+}
 // When the user clicks the button, open the modal 
 closeBtn.onclick = function() {
     modal.style.display = "none";
 }
-
+closeBtnM.onclick = function() {
+    modal.style.display = "none";
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
--- src/header.php
+++ src/header.php
@@ -1,104 +1,121 @@
 <!DOCTYPE html>
 
-<html lang="en">
+<html lang="en"> 
 <head>
-  <link href="http://vjs.zencdn.net/6.2.0/video-js.css" rel="stylesheet">
-  <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/howler/2.0.4/howler.min.js"></script>
-  <!--- basic page needs
-  ================================================== -->
-  <meta charset="utf-8">
-  <title>Scan House Service</title>
-  <meta name="description" content="This template can be used for Scan House">
-  <meta name="Youssef" content="Check accommodation availibility, template Scan house">
+   <link href="http://vjs.zencdn.net/6.2.0/video-js.css" rel="stylesheet">
+   <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/howler/2.0.4/howler.min.js"></script>
+   <!--- basic page needs
+   ================================================== -->
+   <meta charset="utf-8">
+   <title>Scan House Service</title>
+   <meta name="description" content="This template can be used for Scan House">  
+   <meta name="Youssef" content="Check accommodation availibility, template Scan house">
 
-  <!-- mobile specific metas
-  ================================================== -->
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-  <meta name="viewport" content="width=320">
-  <!-- CSS
-  ================================================== -->
-  <link rel="stylesheet" href="css/main.css">
-  <link rel="stylesheet" href="css/animate.css">
-  <link rel="stylesheet" href="css/font-awesome.css">
-  <link rel="stylesheet" type="text/css" href="css/simple-grid.css">
-  <!--     <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="./css/bootstrap-theme.min.css">
+   <!-- mobile specific metas
+   ================================================== -->
+   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+   <meta name="viewport" content="width=320">
+   <!-- CSS
+   ================================================== -->
+   <link rel="stylesheet" href="css/main.css">
+   <link rel="stylesheet" href="css/animate.css">
+   <link rel="stylesheet" href="css/font-awesome.css">
+   <link rel="stylesheet" type="text/css" href="css/simple-grid.css">
+<!--     <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap-theme.min.css"> 
 
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+ 
+  
+
+   <!-- favicons
+   ================================================== -->
+   <link rel="icon" type="image/png" src="/images/favicon.png">
 
 
 
-  <!-- favicons
-  ================================================== -->
-  <link rel="icon" type="image/png" src="/images/favicon.png">
 </head>
 
 <body>
 
-  <!-- header
-  ================================================== -->
-  <div class="menu">
-    <div class="logo-container">
-      <img src="images/logo-test.png" width="157" height="0" alt="Globuzzer" class="logo">
-    </div>
+   <!-- header 
+   ================================================== -->
 
-    <div class="nav-container">
-      <ul class="my-list">
-        <li class="list-item"><a href="index">Home</a></li>
-        <li class="list-item"><a href="about">About</a></li>
-        <li class="list-item"><a href="contact">Contact</a></li>
-        <li id="singIn" class="list-item"><a href="#">Sign in</a></li>
-      </ul>
-    </div>
+      <div class="menu">
 
-    <div class="menu-toggle">
-      <a href="#"><i class="fa fa-bars"  aria-hidden="true"></i></a>
-    </div>
-  </div> <!-- /header -->
-
-  <div id="myModal" class="modal">
-    <!-- Modal content -->
-    <div class="modal-content">
-      <div class="modal-part1">
-        <h2>Have you registred?</h2>
-        <div>
-          <input type="email" name="emailid" placeholder="E-mail">
-          <input type="password" name="passwordid" placeholder="Password">
-          <a href="">Forgot password?</a><br>
-          <a class="primary-button" href="#">Sign in</a>
+        <div class="logo-container">
+         <img src="images/logo-test.png" width="157" height="0" alt="Globuzzer" class="logo">
         </div>
+        
+
+        <div class="nav-container">
+          <ul class="my-list">
+
+          <li class="list-item"><a href="index">Home</a></li>
+          <li class="list-item"><a href="about">About</a></li>
+          <li class="list-item"><a href="contact">Contact</a></li>
+          <li id="singIn" class="list-item"><a href="#">Sign in</a></li>
+          <li id="singInM" class="list-item"><a href="#">Sign in</a></li>
+          <li id="singUpM" class="list-item"><a href="#">Sign up</a></li>
+
+          </ul>
+        </div>
+        <div class="menu-toggle">
+           <a href="#" style="padding:0"><i class="fa fa-bars fa-2x"  aria-hidden="true"></i></a>
+        </div>
+        
+
+        <div id="myModal" class="modal">
+
+       <!-- Modal content -->
+          <div class="modal-content">
+            
+            <div id="part1" class="modal-part1">
+              <h2>Have you registred?</h2>
+              <div class="signIn">
+                <input type="email" name="emailid" placeholder="E-mail">
+                <input type="password" name="passwordid" placeholder="Password">
+                <a href="">Forgot password?</a><br>
+                <a class="primary-button" href="#">Sign in</a><br>
+                <a id="closeM" class="primary-button" href="#">Cancel</a>
+              </div>
+            </div>
+
+            <div id="part2" class="modal-part2">
+              <h2>Not a member?</h2>
+              <div>
+                <div class="signIn-part">
+                  <input type="email" name="emailid" placeholder="E-mail">
+                  <input type="password" name="passwordid" placeholder="Password">
+                </div>
+                <div class="signIn-part">
+                  <input type="text" name="firstname" placeholder="First Name">
+                  <input type="text" name="lastname" placeholder="Last Name">
+                </div>
+              </div>
+              <div class="buttons-side">
+                <a class="modal-prm-btn " href="">Sign In</a>
+                <a class="modal-prm-btn socialF" href="">Log in with <i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+                <a class="modal-prm-btn socialG" id="#signUpButton" href="">Log in with <i class="fa fa-google-plus-square" aria-hidden="true"></i></a>
+              </div>
+              <div><a class="x-button" href="index">&#10006;</a></div>
+            </div>
+            
+            </div>
+             
       </div>
+      
+         
+   </div> <!-- /header --> 
+    
 
-      <div class="modal-part2">
-        <h2>Not a member?</h2>
-        <div>
-          <div class="signIn-part">
-            <input type="email" name="emailid" placeholder="E-mail">
-            <input type="password" name="passwordid" placeholder="Password">
-          </div>
-          <div class="signIn-part">
-            <input type="text" name="firstname" placeholder="First Name">
-            <input type="text" name="lastname" placeholder="Last Name">
-          </div>
-        </div>
-        <div class="buttons-side">
-          <a class="modal-prm-btn" href="">Sign In</a>
-          <a class="modal-prm-btn" href="">Sign In</a>
-          <a class="modal-prm-btn" id="#signUpButton" href="">Sign In</a>
-        </div>
-        <div><a class="x-button" href="index">&#10006;</a></div>
-      </div>
-    </div>
-  </div>
-
-
-  <script>
-  var signUp = document.getElementById("#signUpButton");
-  signUp.onclick = function() {
+<script>
+    var signUp = document.getElementById("#signUpButton");
+    signUp.onclick = function() {
     console.log("Sign Up");
     alert("Hello");
   }
-  </script>
+</script>
